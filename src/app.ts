174dafa99240ddfceec8eccef2ac75f909cc0f45
Katekko <joaovixgon@gmail.com>
2023-05-24 10:58:50 -0300

    Feature multiple commands and multiple args

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import qrcode from 'qrcode-terminal';
 import { Client, LocalAuth } from 'whatsapp-web.js';
+import { commands } from './commands';
 import { Command } from './core/command';
-// import { commands } from './commands';
-import { PingCommand } from './features/common/private/ping.command';
 
 const client = new Client({ authStrategy: new LocalAuth() });
+const commandChar = '!';
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
@@ -15,40 +15,47 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    const command = _findCommand(0, message.body);
-    const args = _findArguments(message.body);
+    const body = message.body;
+    if (!body.startsWith(commandChar)) return null;
+    const commandLine = body.split(commandChar)[1];
+
+    const command = _findCommand(commandLine);
+    const args = _findArguments(commandLine);
 
     if (command != null) command.execute(args);
 });
 
 client.initialize();
 
-function _findArguments(message: string): string[] {
-    const commandChar = '*';
-    if (!message.startsWith(commandChar)) return [];
-
-    const commandLine = message.split(commandChar)[1];
-    return commandLine.split(' ').slice(1);
+function _findArguments(commandLine: string): string[] {
+    const commandParts = commandLine.split(' ');
+    const lastSubcommandIndex = commandParts.findIndex(part => !commands.has(part));
+    return commandParts.slice(lastSubcommandIndex + 1);
 }
 
-function _findCommand(currentIndex: number, message: String): Command | null {
+function _findCommand(commandLine: String): Command | null {
     try {
-        const commandChar = '*';
-        if (message == null || message === '' || !message.startsWith(commandChar)) return null;
-        const getCommandLine = message.split(commandChar)[1];
-        const allArguments = getCommandLine.split(' ');
-        const currentArgument = allArguments[currentIndex];
-        const command = commands.get(currentArgument);
+        const commandParts = commandLine.split(' ');
 
-        if (command == null) throw new Error();
-        if (command instanceof Command) return command;
+        let currentCommand: any = commands;
+        for (let i = 0; i < commandParts.length; i++) {
+            const currentArgument = commandParts[i];
+            const command = currentCommand.get(currentArgument);
 
-        return _findCommand(currentIndex + 1, message);
+            if (command == null) throw new Error();
+
+            if (command instanceof Command) {
+                return command;
+            } else if (command instanceof Map) {
+                currentCommand = command;
+            } else {
+                throw new Error();
+            }
+        }
+
+        throw new Error();
     } catch (error) {
         console.log('command not found');
         return null;
     }
 }
-
-let commands: Map<string, any> = new Map<string, any>();
-commands.set('ping', new PingCommand());
--- src/commands.ts
+++ src/commands.ts
@@ -3,5 +3,6 @@ import { PingCommand } from './features/common/private/ping.command';
 /** All existent commands in the bot */
 export let commands: Map<string, any> = new Map<string, any>();
 commands.set('ping', new PingCommand());
-// commands.set('ping', { 'pong': new PingCommand() });
+commands.set('katekko', new Map<string, any>().set('gostoso', new PingCommand()));
+
 // commands.set('command2', new Command2());
