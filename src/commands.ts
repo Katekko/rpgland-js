0dc8f819238f456f07dd47bdc236f4267c591850
Katekko <joaovixgon@gmail.com>
2023-05-25 16:15:52 -0300

    Fix spelling shop buy command

--- src/commands.ts
+++ src/commands.ts
@@ -5,7 +5,7 @@ import { PerfilCommand } from './features/rpg/both/perfil.command';
 import { HuntCommand } from './features/rpg/private/hunt/hunt.command';
 import { HuntAttackCommand } from './features/rpg/private/hunt/hunt_attack.command';
 import { ShopCommand } from './features/rpg/private/shop/shop.command';
-import { ShopBuyCommand } from './features/rpg/private/shop/shop_but.command';
+import { ShopBuyCommand } from './features/rpg/private/shop/shop_buy.command';
 import { StartCommand } from './features/rpg/private/start.command';
 
 /** All existent commands in the bot */
