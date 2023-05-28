ae2df3e04921768fafb3caddf58cc3e99ced3f1f
Katekko <joaovixgon@gmail.com>
2023-05-28 15:06:53 -0300

    Fix command for only private

--- src/core/middlewares/command_only_for_private.middleware.ts
+++ src/core/middlewares/command_only_for_private.middleware.ts
@@ -1,9 +1,18 @@
 import { Message } from "whatsapp-web.js";
-import { i18n } from "../../i18n/translation";
+import { CommandTranslations, i18n } from "../../i18n/translation";
+import { ServiceFactory } from "../factories/service.factory";
 
 export async function commandOnlyForPrivate(message: Message): Promise<boolean> {
     if ((await message.getChat()).isGroup) {
-        const translate = i18n((await message.getContact()).number);
+        const playersService = ServiceFactory.makePlayersService();
+        const player = await playersService.getPlayerByMessage(message);
+        let translate: CommandTranslations;
+        if (player) {
+            translate = i18n(player.language);
+        } else {
+            translate = i18n((await message.getContact()).number);
+        }
+
         message.reply(translate.commands.commons.commandOnlyForPrivate);
         return false;
     }
